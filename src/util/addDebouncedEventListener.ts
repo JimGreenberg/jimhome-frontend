912d728e6d673f634e21addd524805495d669c47
Jim Greenberg <jim@onsip.com>
2021-05-08 17:12:37 -0400

    add addDebouncedEventListener to WebSocket prototype as well

--- src/util/addDebouncedEventListener.ts
+++ src/util/addDebouncedEventListener.ts
@@ -1,17 +1,22 @@
 export {}; // moduleize this file
+interface WithAddDebouncedEventListener {
+  addDebouncedEventListener: typeof addDebouncedEventListener;
+}
 
-function addDebouncedEventListener <K extends keyof HTMLElementEventMap>(this: HTMLElement, type: K, debounceTime: number, listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void {
+declare global {
+  interface HTMLElement extends WithAddDebouncedEventListener {}
+  interface WebSocket extends WithAddDebouncedEventListener {}
+}
+
+function addDebouncedEventListener<K extends keyof WebSocketEventMap>(this: WebSocket, type: K, debounceTime: number, listener: (this: WebSocket, ev: WebSocketEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
+function addDebouncedEventListener <K extends keyof HTMLElementEventMap>(this: HTMLElement, type: K, debounceTime: number, listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
+function addDebouncedEventListener <K>(this: any, type: K, debounceTime: number, listener: (this: any, ev: any) => any, options?: boolean | AddEventListenerOptions): void {
   let currentTimeout: ReturnType<typeof setTimeout>;
-  this.addEventListener(type, event => {
+  this.addEventListener(type, (event: any) => {
     currentTimeout && window.clearTimeout(currentTimeout);
     currentTimeout = setTimeout(listener.bind(this, event), debounceTime);
   })
 };
 
-declare global {
-  interface HTMLElement {
-    addDebouncedEventListener: typeof addDebouncedEventListener;
-  }
-}
-
 HTMLElement.prototype.addDebouncedEventListener = addDebouncedEventListener;
+WebSocket.prototype.addDebouncedEventListener = addDebouncedEventListener;
